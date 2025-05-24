4f0eec1f1c583ca1c6a1cd821074a08683919f7d
Adititripathi241 <120390686+Adititripathi241@users.noreply.github.com>
2025-05-25 01:00:46 +0530

    final

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-  origin: "http://localhost:5173",
+  origin:[ "http://localhost:5173","https://second-finalchatapp-1.onrender.com"],
   credentials:true
 };
 app.use(cors(corsOption));
